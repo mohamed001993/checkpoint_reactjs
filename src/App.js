03b504ddcbd77e8ad8e3934ec433bc0fd132122b
mohamed001993 <nour001920@gmail.com>
2024-01-17 22:43:36 +0100

    first react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import Mohamed from "./Card";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./index.css"
+import Crd from "./Crd";
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="App"> 
+     <Mohamed/>
+     <header className="Crd">
+    <Crd/>
+    <Crd/>
+    <Crd/>
+    </header>
     </div>
+    
+    
+    
+    
+    
+    
+    </>
+
+
+
+
+
   );
 }
 
